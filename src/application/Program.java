022694da62a001156dba355edd8b2819103476a5
Barbozha <barbozha@.gmail.com>
2021-07-23 15:28:40 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,23 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(1, "Alex Green", "alex@gmail.com", sdf.parse("16/07/1966"), 3000.00, obj);
+		//SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		//Department obj = new Department(1, "Books");
+		//Seller seller = new Seller(1, "Alex Green", "alex@gmail.com", sdf.parse("16/07/1966"), 3000.00, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	// Terá operações estáticas para implementar o DAO
 	// O createSellerDao terá que retornar um SellerDao
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC(); //retorna um SellerDaoJDBC
+		return new SellerDaoJDBC(DB.getConnection()); //retorna um SellerDaoJDBC
 		// Desta forma, a minha classe DaoFactory vai expor um método que retorna
 		// o tipo da interface(SellerDao) mas internamente ela vai instanciar
 		// um implementação(return new SellerDaoJDBC())
