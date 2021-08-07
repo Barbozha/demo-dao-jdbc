ddcff13b5ec951b7207dedd32b2ffb680a450638
Barbozha <barbozha@.gmail.com>
2021-08-07 16:59:50 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserido! Novo id = " + newSeller.getId());
 
+		System.out.println("\n=== TESTE 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Realizado com Sucesso!");
+		
 
 	}
 
